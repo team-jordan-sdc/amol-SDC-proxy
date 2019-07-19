ff601a851b0ee01311d43ab1f9a861580a81271e
Will Resen <willresen@gmail.com>
2019-07-18 17:15:53 -0700

    Replace service hostnames with environment variables

--- server/index.js
+++ server/index.js
@@ -3,16 +3,16 @@ const proxy = require('express-http-proxy');
 const app = express();
 const port = 8080;
 
-
 app.use(express.static('./public'));
 
-app.use('/api/movies', proxy('localhost:3000', { proxyReqPathResolver: (req) => `/api/movies${req.url}`}));
-app.use('/api/personnel', proxy('localhost:3000', { proxyReqPathResolver: (req) => `/api/personnel${req.url}`}));
+app.use('/api/movies', proxy(process.env.CASTCREW_ADDR, { proxyReqPathResolver: (req) => `/api/movies${req.url}`}));
+app.use('/api/personnel', proxy(process.env.CASTCREW_ADDR, { proxyReqPathResolver: (req) => `/api/personnel${req.url}`}));
 
-app.use('/api/reviews', proxy('localhost:3001', { proxyReqPathResolver: (req) => `/api/reviews${req.url}`}));
-app.use('/api/products', proxy('localhost:3001', { proxyReqPathResolver: (req) => `/api/products${req.url}`}));
+app.use('/api/reviews', proxy(process.env.REVIEWS_ADDR, { proxyReqPathResolver: (req) => `/api/reviews${req.url}`}));
+app.use('/api/products', proxy(process.env.REVIEWS_ADDR, { proxyReqPathResolver: (req) => `/api/products${req.url}`}));
 
-app.use('/api/featured_movie', proxy('localhost:3002', { proxyReqPathResolver: (req) => req.url}));
-app.use('/api/rand', proxy('localhost:3002', { proxyReqPathResolver: (req) => `/api/rand${req.url}`}));
+app.use('/api/featured_movie', proxy(process.env.FEATURED_ADDR, { proxyReqPathResolver: (req) => req.url}));
+app.use('/api/featured', proxy(process.env.FEATURED_ADDR, { proxyReqPathResolver: (req) => `/api/featured${req.url}`}));
+app.use('/api/rand', proxy(process.env.FEATURED_ADDR, { proxyReqPathResolver: (req) => `/api/rand${req.url}`}));
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
